35c9b3d9ae79dfecddbf575034ad434667c8f8e8
fasiondog <fasiondog@fasiondogdeMac.local>
2018-09-20 23:12:30 +0800

    update xmake

--- hikyuu_pywrap/xmake_on_load.lua
+++ hikyuu_pywrap/xmake_on_load.lua
@@ -8,22 +8,21 @@ function main(target)
         return
     end
     
-    local python = "python" .. get_config("python")
-
     if is_plat("macosx") then
-        local libdir = os.iorun(format("%s-config --prefix", python))
-        local lcPos = string.find(libdir,"\n")
-        libdir = (string.sub(libdir,1,lcPos-1).."/lib")
+        local libdir = os.iorun("python3-config --prefix"):trim() .. "/lib"
         target:add("linkdirs", libdir)
-        target:add("links", python.."m")
+        local out, err = os.iorun("python3 --version")
+        local ver = (out .. err):trim()
+        local python_lib = format("python%s.%sm", string.sub(ver,8,8), string.sub(ver,10,10))
+        target:add("links", python_lib)
     end
 
     target:add("rpathdirs", "$ORIGIN", "$ORIGIN/lib", "$ORIGIN/../lib")
 
     -- get python include directory.
-    local pydir = os.iorun(format("%s-config --includes", python))
-    local lcPos = string.find(pydir,"\n")
-    pydir = (string.sub(pydir,1,lcPos-1))
+    local pydir = os.iorun("python3-config --includes"):trim()
+    --local lcPos = string.find(pydir,"\n")
+    --pydir = (string.sub(pydir,1,lcPos-1))
     target:add("cxflags", pydir)
 
     -- get suffix configure for link libboost_pythonX.so
@@ -34,11 +33,9 @@ function main(target)
 
     suffix = string.upper(suffix)
     if suffix == "3X" then
-        --此处奇怪,macOSX下print(os.iorun("python3 --version"))正常，但赋值给变量的话，变量长度一直为0
-        --local ver = os.iorun("python3 --version")
-        --local boost_python_lib = "boost_python"..string.sub(ver,8,8)..string.sub(ver,10,10)
-        local ver = get_config("python")
-        local boost_python_lib = "boost_python" .. string.sub(ver,1,1) .. string.sub(ver,3,3)
+        local out, err = os.iorun("python3 --version")
+        local ver = (out .. err):trim()
+        local boost_python_lib = "boost_python"..string.sub(ver,8,8)..string.sub(ver,10,10)
         target:add("links", boost_python_lib)
     else
         target:add("links", "boost_python"..suffix)
