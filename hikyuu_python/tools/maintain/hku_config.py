c70d8f0d78ce11cd2ccefae8019d3dbb209bd762
fasiondog <fasiondog@163.com>
2018-10-17 02:15:56 +0800

    data maintain tool(continue)

--- hikyuu_python/tools/maintain/hku_config.py
+++ hikyuu_python/tools/maintain/hku_config.py
@@ -13,8 +13,9 @@ import logging
 
 from configparser import ConfigParser
 
-from MainWindow import *
+from pytdx.config.hosts import hq_hosts
 
+from MainWindow import *
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -29,131 +30,67 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowIcon(QIcon("./hikyuu.ico"))
         self.setFixedSize(self.width(), self.height())
 
-        #从用户目录下子目录.hikyuu下"data_dir.ini"文件中获取目标数据存放路径
-        usr_dir = os.path.expanduser('~')
-        hku_dir = usr_dir + '/.hikyuu'
+        #读取保存的配置文件信息，如果不存在，则使用默认配置
+        this_dir = os.getcwd()
+        import_config = ConfigParser()
+        if os.path.exists(this_dir + '/importdata.cfg'):
+            import_config.read(this_dir + '/importdata.ini')
 
-        if not os.path.lexists(hku_dir):
-            os.mkdir(hku_dir)
+        #初始化通道信目录配置
+        tdx_enable = import_config.getboolean('tdx', 'enable', fallback=True)
+        tdx_dir = import_config.get('tdx', 'dir', fallback='d:\TdxW_HuaTai')
+        self.tdx_enable_checkBox.setChecked(tdx_enable)
+        self.tdx_dir_lineEdit.setText(tdx_dir)
+        import_config['tdx'] = {'enable': tdx_enable, 'dir': tdx_dir}
 
-        data_dir_config = ConfigParser()
-        data_config_file = hku_dir + '/data_dir.ini'
-        try:
-            data_dir_config.read(data_config_file)
-        except:
-            data_dir_config['data_dir'] = {}
+        #初始化大智慧目录配置
+        dzh_enable = import_config.getboolean('dzh', 'enable', fallback=False)
+        dzh_dir = import_config.get('dzh', 'dir', fallback='')
+        self.dzh_checkBox.setChecked(dzh_enable)
+        self.dzh_dir_lineEdit.setText(dzh_dir)
+        import_config['dzh'] = {'enable': dzh_enable, 'dir': dzh_dir}
 
-        if 'data_dir' not in data_dir_config['data_dir']:
-            data_dir_config['data_dir']['data_dir'] = "c:\stock" if sys.platform == "win32" else usr_dir + "/stock"
+        #初始化pytdx配置及显示
+        tdx_server = import_config.get('pytdx', 'server', fallback='招商证券深圳行情')
+        self.tdx_servers_comboBox.setDuplicatesEnabled(True)
+        default_tdx_index = 0
+        for i, host in enumerate(hq_hosts):
+            self.tdx_servers_comboBox.addItem(host[0], host[1])
+            if host[0] == tdx_server:
+                default_tdx_index = i
+        self.tdx_servers_comboBox.setCurrentIndex(default_tdx_index)
+        self.tdx_port_lineEdit.setText(str(hq_hosts[default_tdx_index][2]))
+        import_config['pytdx'] = {'server': tdx_server,
+                                  'ip': hq_hosts[default_tdx_index][1],
+                                  'port': hq_hosts[default_tdx_index][2]}
 
-        data_dir = data_dir_config['data_dir']['data_dir']
+        #初始化hdf5设置
+        hdf5_enable = import_config.getboolean('hdf5', 'enable', fallback=True)
+        hdf5_dir = import_config.get('hdf5', 'dir', fallback="c:\stock" if sys.platform == "win32" else os.path.expanduser('~') + "/stock")
+        self.hdf5_enable_checkBox.setChecked(hdf5_enable)
+        self.hdf5_dir_lineEdit.setText(hdf5_dir)
+        import_config['hdf5'] = {'enable': hdf5_enable, 'dir': hdf5_dir}
 
-        #读取importdata.ini配置信息
-        import_config_file = data_dir + '/importdata.ini'
-        try:
-            import_config = self.load_import_config(import_config_file)
-        except:
-            import_config = self.get_default_hku_config(data_dir)
+        #初始化MYSQL设置
+        mysql_enable = import_config.getboolean('mysql', 'enable', fallback=False)
+        mysql_host = import_config.get('mysql', 'host', fallback='127.0.0.1')
+        mysql_port = import_config.get('mysql', 'port', fallback='8809')
+        mysql_usr = import_config.get('mysql', 'usr', fallback='root')
+        mysql_pwd = import_config.get('mysql', 'pwd', fallback='')
+        self.mysql_enable_checkBox.setChecked(mysql_enable)
+        self.mysql_host_lineEdit.setText(mysql_host)
+        self.mysql_port_lineEdit.setText(mysql_port)
+        self.mysql_usr_lineEdit.setText(mysql_usr)
+        self.mysql_pwd_lineEdit.setText(mysql_pwd)
+        import_config['mysql'] = {'enable': mysql_enable, 'host': mysql_host, 'port': mysql_port,
+                                  'usr': mysql_usr, 'pwd': mysql_pwd}
 
-        from_list = []
-        option_list = import_config.options('from')
-        for option in option_list:
-            tmp = option.split(',')
-            if tmp[0] == "tdx":
-                break
-
-
-
-        if sys.platform == 'win32':
-            hku_config_file = data_dir + '/hikyuu_win.ini'
-        else:
-            hku_config_file = data_dir + '/hikyuu_linux.ini'
-
-        try:
-            hku_config = self.load_hku_config(hku_config_file)
-        except:
-            hku_config = self.get_default_hku_config(data_dir)
-
-        self.dataDirLineEdit.setText(data_dir)
-        self.data_config_file = data_config_file
-        self.data_dir_config = data_dir_config
 
 
     def load_import_config(self, filename):
         import_config = ConfigParser()
         import_config.read(filename)
         return import_config
-
-    def get_default_import_config(self, data_dir):
-        import_config = ConfigParser()
-        import_config['database'] = {}
-        import_config['database']['db'] = data_dir + '/stock.db'
-        import_config['database']['sql'] = data_dir + '/create.sql'
-
-        import_config['weight'] = {}
-        import_config['weight']['sh'] = data_dir + '/weight/shase/weight'
-        import_config['weight']['sz'] = data_dir + '/weight/sznse/weight'
-
-        import_config['from'] = {}
-        import_config['from'] = r'1, tdx, d:\TdxW_HuaTai'
-
-        import_config['dest'] = {}
-        import_config['dest']['sh_day'] = data_dir + '/sh_day.h5'
-        import_config['dest']['sh_1min'] = data_dir + '/sh_1min.h5'
-        import_config['dest']['sh_5min'] = data_dir + '/sh_5min.h5'
-        import_config['dest']['sz_day'] = data_dir + '/sz_day.h5'
-        import_config['dest']['sz_1min'] = data_dir + '/sz_1min.h5'
-        import_config['dest']['sz_5min'] = data_dir + '/sz_5min.h5'
-        return import_config
-
-
-
-    def load_hku_config(self, filename):
-        hku_config = ConfigParser()
-        hku_config.read(filename)
-        return hku_config
-
-    def get_default_hku_config(self, data_dir):
-        hku_config = ConfigParser()
-        hku_config['hikyuu'] = {}
-        hku_config['hikyuu']['tmpdir'] = data_dir
-
-        hku_config['block'] = {}
-        hku_config['block']['type'] = 'qianlong'
-        hku_config['block']['dir'] = data_dir + '/block'
-        hku_config['block']['指数板块'] = 'zsbk.ini'
-        hku_config['block']['行业板块'] = 'hybk.ini'
-        hku_config['block']['地域板块'] = 'dybk.ini'
-        hku_config['block']['概念板块'] = 'gnbk.ini'
-        hku_config['block']['self'] = 'self.ini'
-
-        hku_config['preload'] = {}
-        hku_config['preload']['day'] = 1
-        hku_config['preload']['week'] = 0
-        hku_config['preload']['month'] = 0
-        hku_config['preload']['quater'] = 0
-        hku_config['preload']['halfyear'] = 0
-        hku_config['preload']['year'] = 0
-        hku_config['preload']['min'] = 0
-        hku_config['preload']['min5'] = 0
-        hku_config['preload']['min15'] = 0
-        hku_config['preload']['min30'] = 0
-        hku_config['preload']['min60'] = 0
-
-        hku_config['baseinfo'] = {}
-        hku_config['baseinfo']['type'] = 'sqlite3'
-        hku_config['baseinfo']['db'] = data_dir + '/stock.db'
-
-        hku_config['kdata'] = {}
-        hku_config['kdata']['type'] = 'hdf5'
-        hku_config['kdata']['sh_day'] = data_dir + '/sh_day.h5'
-        hku_config['kdata']['sh_min'] = data_dir + '/sh_1min.h5'
-        hku_config['kdata']['sh_min5'] = data_dir + '/sh_5min.h5'
-        hku_config['kdata']['sz_day'] = data_dir + '/sz_day.h5'
-        hku_config['kdata']['sz_min'] = data_dir + '/sz_1min.h5'
-        hku_config['kdata']['sz_min5'] = data_dir + '/sz_5min.h5'
-        return hku_config
-
 
 
 if __name__ == "__main__":
