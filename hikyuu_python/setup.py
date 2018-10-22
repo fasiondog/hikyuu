2a8b8b4ef79669203f977e52bc690836bf9489ca
fasiondog <fasiondog@163.com>
2018-10-23 01:09:13 +0800

    release V1.0.9

--- hikyuu_python/setup.py
+++ hikyuu_python/setup.py
@@ -7,11 +7,12 @@ import os
 
 if sys.argv[-1] == 'publish':
     os.system("python setup.py bdist_wheel --python-tag cp35 -p win-amd64")
+    #python setup.py bdist_wheel --python-tag cp35 -p linux_x86_64
     os.system("twine upload dist/*")
     sys.exit()
 
 hku_name = "Hikyuu"
-hku_version = "1.0.8"
+hku_version = "1.0.9"
 hku_author = "fasiondog"
 hku_author_email = "fasiondog@sina.com"
 
@@ -29,23 +30,20 @@ if sys.platform == 'win32':
                       ('Lib/site-packages/hikyuu', ['hikyuu/hikyuu_utils.dll']),
                       ('Lib/site-packages/hikyuu', ['hikyuu/sqlite3.dll']),
                       ('Lib/site-packages/hikyuu', ['hikyuu/importdata.exe']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_chrono-vc100-mt-1_56.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_date_time-vc100-mt-1_56.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_filesystem-vc100-mt-1_56.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_python-vc100-mt-1_56.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_serialization-vc100-mt-1_56.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_system-vc100-mt-1_56.dll']),
-                      #('Lib/site-packages/hikyuu', ['hikyuu/boost_thread-vc100-mt-1_56.dll']),
-                      #('Lib/site-packages/hikyuu', ['hikyuu/boost_unit_test_framework-vc100-mt-1_56.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/log4cplus.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5dll.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5_hldll.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5_cppdll.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5_hl_cppdll.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_date_time-vc141-mt-x64-1_68.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_filesystem-vc141-mt-x64-1_68.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_python35-vc141-mt-x64-1_68.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_serialization-vc141-mt-x64-1_68.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/boost_system-vc141-mt-x64-1_68.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/libmysql.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5_hl.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5_cpp.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/hdf5_hl_cpp.dll']),
                       ('Lib/site-packages/hikyuu', ['hikyuu/szip.dll']),
                       ('Lib/site-packages/hikyuu', ['hikyuu/zlib.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/msvcp100.dll']),
-                      ('Lib/site-packages/hikyuu', ['hikyuu/msvcr100.dll'])]
+                      ('Lib/site-packages/hikyuu', ['hikyuu/concrt140.dll']),
+                      ('Lib/site-packages/hikyuu', ['hikyuu/vcruntime140.dll'])]
 else:
     hku_data_files = []
 
@@ -66,7 +64,7 @@ setup(
         zip_safe = False,
         include_package_data=True,
         package_data = {
-            '': ['*.rst', '*.pyd', '*.ini', '*.sql', '*.properties', '*.xml', 'LICENSE.txt'],
+            '': ['*.rst', '*.pyd', '*.ini', '*.sql', '*.properties', '*.xml', 'LICENSE.txt', 'lib*.so.*'],
         },
         
         data_files = hku_data_files,
