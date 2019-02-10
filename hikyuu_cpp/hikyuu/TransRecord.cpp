f3b961240334b3442b3e2ef5a2e82e7fe521f7bf
fasiondog <fasiondog@163.com>
2019-02-10 23:31:27 +0800

    增加分笔成交数据支持(continue)

---
+++ hikyuu_cpp/hikyuu/TransRecord.cpp
@@ -0,0 +1,21 @@
+/*
+ * TransRecord.cpp
+ *
+ *  Created on: 2019年2月10日
+ *      Author: fasiondog
+ */
+
+#include "TransRecord.h"
+
+namespace hku {
+
+TransRecord::TransRecord() {
+    // TODO Auto-generated constructor stub
+
+}
+
+TransRecord::~TransRecord() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace hku */
---
+++ hikyuu_cpp/hikyuu/TransRecord.h
@@ -0,0 +1,26 @@
+/*
+ * TransRecord.h
+ *
+ *  Created on: 2019年2月10日
+ *      Author: fasiondog
+ */
+
+#ifndef TRANSRECORD_H_
+#define TRANSRECORD_H_
+
+namespace hku {
+
+class TransRecord {
+public:
+    Datetime datetime;  //时间
+    price_t price;      //成交均价
+    price_t vol;        //成交量
+    hku_int buyorsell;  //买卖盘性质：1--sell 0--buy 2--集合竞价
+
+    TransRecord();
+    virtual ~TransRecord();
+};
+
+} /* namespace hku */
+
+#endif /* TRANSRECORD_H_ */
