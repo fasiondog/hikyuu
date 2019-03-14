5bf64446a549202574f493953546115396a91d12
fasiondog <fasiondog@163.com>
2019-03-15 00:56:39 +0800

    增加些预定义的板块全局变量，方便使用

--- hikyuu/interactive/interactive.py
+++ hikyuu/interactive/interactive.py
@@ -132,22 +132,36 @@ blocka = Block("A", "ALL")
 for s in sm:
     if s.type in (constant.STOCKTYPE_A, constant.STOCKTYPE_GEM):
         blocka.add(s)
+zsbk_a = blocka
 
 blocksh = Block("A", "SH")
 for s in blocka:
     if s.market == "SH":
         blocksh.add(s)
+zsbk_sh = blocksh
 
 blocksz = Block("A", "SZ")
 for s in blocka:
     if s.market == "SZ":
         blocksz.add(s)
+zsbk_sz = blocksz
 
-blockg = Block("G", "GEM")
+blockg = Block("G", "创业板")
 for s in sm:
     if s.type == constant.STOCKTYPE_GEM:
         blockg.add(s)
- 
+zsbk_cyb = blockg
+
+blockzxb = Block("A", "中小板")
+for s in blocksz:
+    if s.code[:3] == "002":
+        blockzxb.add(s) 
+zsbk_zxb = blockzxb
+
+zsbk_sz50 = sm.getBlock("指数板块", "上证50")
+zsbk_sz180 = sm.getBlock("指数板块", "上证180")
+zsbk_hs300 = sm.getBlock("指数板块", "沪深300")
+zsbk_zz100 = sm.getBlock("指数板块", "沪深300")
         
 #==============================================================================
 #
