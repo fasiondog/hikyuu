726c21f24a36819227841f15dbf2bbc3f8d30608
fasiondog <fasiondog@163.com>
2019-04-02 23:26:59 +0800

    get the history finance Info (continue)

---
+++ hikyuu_cpp/hikyuu/data_driver/HistoryFinanceReader.cpp
@@ -0,0 +1,26 @@
+/*
+ * HistoryFinanceReader.cpp
+ * 
+ * Copyright (c) 2019 fasiondog
+ *
+ *  Created on: 2019年4月2日
+ *      Author: fasiondog
+ */
+
+#include "HistoryFinanceReader.h"
+
+namespace hku {
+
+HistoryFinanceReader::HistoryFinanceReader(const string& dir)
+: m_dir(dir) {
+
+}
+
+HistoryFinanceReader::~HistoryFinanceReader() {
+
+}
+
+} /* namespace hku */
+
+
+
---
+++ hikyuu_cpp/hikyuu/data_driver/HistoryFinanceReader.h
@@ -0,0 +1,34 @@
+/*
+ * HistoryFinanceReader.h
+ * 
+ * Copyright (c) 2019 fasiondog
+ *  
+ *  Created on: 2019-4-2
+ *      Author: fasiondog
+ */
+
+#ifndef HISTORYFINANCEREADER_H_
+#define HISTORYFINANCEREADER_H_
+
+#include "../DataType.h"
+
+namespace hku {
+
+/**
+ * 读取历史财务信息
+ */
+class HKU_API HistoryFinanceReader {
+
+public:
+    HistoryFinanceReader() = delete;
+    HistoryFinanceReader(const string& dir);
+    virtual ~HistoryFinanceReader();
+
+private:
+    string m_dir;  //历史财务信息文件存放目录
+};
+
+
+} /* namespace */
+
+#endif /* HISTORYFINANCEREADER_H_ */
