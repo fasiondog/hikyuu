b864bed41298853e2ae66dddd59daf088cae5921
fasiondog <fasiondog@163.com>
2019-04-06 20:03:22 +0800

    原1.1.0发布说明中版本号错误

--- setup.py
+++ setup.py
@@ -298,6 +298,8 @@ setup(
             # that you indicate whether you support Python 2, Python 3 or both.
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
         ],
 
         entry_points={
