9bb53a8b74d7492c54eb4353f04a109f8b6c7817
fasiondog <fasiondog@163.com>
2019-04-11 22:09:09 +0800

    修改Linux下log(0)和Windows返回值不同问题

--- hikyuu_cpp/unit_test/libs/hikyuu/indicator/test_LOG.cpp
+++ hikyuu_cpp/unit_test/libs/hikyuu/indicator/test_LOG.cpp
@@ -42,7 +42,8 @@ BOOST_AUTO_TEST_CASE( test_LOG ) {
     BOOST_CHECK(result.name() == "LOG");
     BOOST_CHECK(result.size() == 11);
     BOOST_CHECK(result.discard() == 0);
-    for (int i = 0; i <10; ++i) {
+    BOOST_CHECK(result[0] == Null<price_t>());
+    for (int i = 1; i <10; ++i) {
         BOOST_CHECK_CLOSE(result[i], std::log10(i), 0.00001);
     }
     BOOST_CHECK(result[10] == Null<price_t>());
