1ed963e93a55bdda68bb13ec9d502980019d3781
fasiondog <fasiondog@163.com>
2019-05-11 22:52:31 +0800

    add SUMBARS indicator

--- hikyuu_cpp/hikyuu/indicator/imp/ISumBars.cpp
+++ hikyuu_cpp/hikyuu/indicator/imp/ISumBars.cpp
@@ -46,40 +46,26 @@ void ISumBars::_calculate(const Indicator& ind) {
         return;
     }
 
-    if (total == m_discard + 2) {
-        double sum = ind[m_discard] + ind[m_discard+1];
-        if (sum >= a) {
-            _set(ind[m_discard+1] >= a ? 0 : 1, m_discard+1);
-            if (ind[m_discard] >= a) {
-                _set(0, m_discard);
-            } else {
-                m_discard += 1;
-            }
-        } else if (ind[m_discard] >= a) {
-            _set(0, m_discard);
-        } else {
-            m_discard = total;
-        }
-        return;
-    }
-
-    size_t pos = total - 1;
-    size_t last_pos = pos;
+    size_t start = total - 1;
+    size_t pos = start;
+    size_t last_pos = start;
     double sum = ind[pos];
-    if (sum >= a) {
-        _set(0, total-1);
-    }
-    size_t start = total - 2;
     for (size_t i = start; i >= m_discard; i--) {
-        sum = sum - ind[i+1];
+        if (i != start) {
+            sum = sum - ind[i+1];
+        }
+
+        if (i < pos) {
+            sum = ind[i];
+            pos = i;
+        }
+
         if (sum < a) {
             if (pos >= 1) {
-                for (size_t j = pos - 1; j >= m_discard; j--) {
-                    std::cout << "i: " << i << ", j: " << j << std::endl;
+                for (size_t j = pos-1; j >= m_discard; j--) {
                     sum += ind[j];
                     if (sum >= a) {
                         pos = j;
-                        //last_pos = j;
                         break;
                     }
 
@@ -88,33 +74,22 @@ void ISumBars::_calculate(const Indicator& ind) {
                         break;
                     }
                 }
-            }
-        } else {
-            if (i < pos) {
-                pos = i;
-                sum += ind[i];
             } else {
                 pos = Null<size_t>();
             }
         }
 
-        //std::cout << "i: " << i << ", pos: " << pos << ", last_pos: " << last_pos << std::endl;
         if (pos != Null<size_t>()) {
             _set(i - pos, i);
-        } else {
+        }
+
+        if (i == m_discard || pos == Null<size_t>()) {
             last_pos = i;
             break;
         }
-
-        if (i == m_discard) {
-            break;
-        }
     }
 
-    std::cout << "pos: " << pos << ", last_pos: " << last_pos << std::endl;
-    if (pos != Null<price_t>()) {
-        m_discard = pos;
-    }
+    m_discard = pos == Null<size_t>() ? last_pos + 1 : last_pos;
 }
 
 
--- hikyuu_cpp/unit_test/libs/hikyuu/indicator/test_SUMBARS.cpp
+++ hikyuu_cpp/unit_test/libs/hikyuu/indicator/test_SUMBARS.cpp
@@ -16,6 +16,7 @@
 
 #include <fstream>
 #include <hikyuu/StockManager.h>
+#include <hikyuu/indicator/crt/CVAL.h>
 #include <hikyuu/indicator/crt/SUMBARS.h>
 #include <hikyuu/indicator/crt/KDATA.h>
 #include <hikyuu/indicator/crt/PRICELIST.h>
@@ -41,11 +42,25 @@ BOOST_AUTO_TEST_CASE( test_SUMBARS ) {
 
     result = SUMBARS(data, 10);
     BOOST_CHECK(result.name() == "SUMBARS");
-#if 0
     BOOST_CHECK(result.size() == data.size());
     BOOST_CHECK(result.discard() == 1);
     BOOST_CHECK(result[0] == Null<price_t>());
     BOOST_CHECK(result[1] == 0);
+    BOOST_CHECK(result[2] == 0);
+    BOOST_CHECK(result[3] == 0);
+    BOOST_CHECK(result[4] == 0);
+    BOOST_CHECK(result[5] == 0);
+    BOOST_CHECK(result[6] == 0);
+    BOOST_CHECK(result[7] == 0);
+    BOOST_CHECK(result[8] == 0);
+    BOOST_CHECK(result[9] == 0);
+
+    result = SUMBARS(data, 15);
+    BOOST_CHECK(result.name() == "SUMBARS");
+    BOOST_CHECK(result.size() == data.size());
+    BOOST_CHECK(result.discard() == 2);
+    BOOST_CHECK(result[0] == Null<price_t>());
+    BOOST_CHECK(result[1] == Null<price_t>());
     BOOST_CHECK(result[2] == 0);
     BOOST_CHECK(result[3] == 0);
     BOOST_CHECK(result[4] == 0);
@@ -83,61 +98,70 @@ BOOST_AUTO_TEST_CASE( test_SUMBARS ) {
     BOOST_CHECK(result[7] == 0);
     BOOST_CHECK(result[8] == 0);
     BOOST_CHECK(result[9] == 0);
-    std::cout << result.discard() << std::endl;
-    for (size_t i = 0; i < result.size(); i++) {
-        std::cout << i << ": " << data[i] << ", " << result[i] << std::endl;
+
+    result = SUMBARS(data, 170);
+    BOOST_CHECK(result.discard() == 6);
+    BOOST_CHECK(result.size() == 10);
+    for (size_t i = 0; i < result.discard(); i++) {
+        BOOST_CHECK(result[i] == Null<price_t>());
+    }
+    BOOST_CHECK(result[6] == 3);
+    BOOST_CHECK(result[7] == 2);
+    BOOST_CHECK(result[8] == 2);
+    BOOST_CHECK(result[9] == 1);
+
+    result = SUMBARS(data, 450);
+    BOOST_CHECK(result.size() == 10);
+    BOOST_CHECK(result.discard() == 9);
+    for (size_t i = 0; i < result.discard(); i++) {
+        BOOST_CHECK(result[i] == Null<price_t>());
+    }
+    BOOST_CHECK(result[9] == 8);
+
+    result = SUMBARS(data, 451);
+    BOOST_CHECK(result.size() == 10);
+    BOOST_CHECK(result.discard() == 10);
+    for (size_t i = 0; i < result.discard(); i++) {
+        BOOST_CHECK(result[i] == Null<price_t>());
     }
 
-
-
-    BOOST_CHECK(result.discard() == 3);
+    result = SUMBARS(CVAL(10), 451);
+    BOOST_CHECK(result.discard() == 1);
+    BOOST_CHECK(result.size() == 1);
     BOOST_CHECK(result[0] == Null<price_t>());
-    BOOST_CHECK(result[1] == Null<price_t>());
-    BOOST_CHECK(result[2] == Null<price_t>());
-    BOOST_CHECK(result[3] == 0);
-    BOOST_CHECK(result[4] == 1);
-    BOOST_CHECK(result[5] == 2);
-    BOOST_CHECK(result[6] == 0);
 
-    a.insert(a.begin(), 1);
-    data = PRICELIST(a);
-    result = BARSLAST(data);
-    BOOST_CHECK(result.size() == data.size());
-    BOOST_CHECK(result.discard() == 0);
-    BOOST_CHECK(result[0] == 0);
-    BOOST_CHECK(result[1] == 1);
-    BOOST_CHECK(result[2] == 2);
-    BOOST_CHECK(result[3] == 3);
-    BOOST_CHECK(result[4] == 0);
-    BOOST_CHECK(result[5] == 1);
-    BOOST_CHECK(result[6] == 2);
-    BOOST_CHECK(result[7] == 0);
-
-    a.push_back(0.0);
-    data = PRICELIST(a);
-    result = BARSLAST(data);
-    BOOST_CHECK(result.size() == data.size());
-    BOOST_CHECK(result.discard() == 0);
-    BOOST_CHECK(result[0] == 0);
-    BOOST_CHECK(result[1] == 1);
-    BOOST_CHECK(result[2] == 2);
-    BOOST_CHECK(result[3] == 3);
-    BOOST_CHECK(result[4] == 0);
-    BOOST_CHECK(result[5] == 1);
-    BOOST_CHECK(result[6] == 2);
-    BOOST_CHECK(result[7] == 0);
-    BOOST_CHECK(result[8] == 1);
-
-    result = BARSLAST(-11);
+    result = SUMBARS(CVAL(10), 10);
     BOOST_CHECK(result.size() == 1);
     BOOST_CHECK(result.discard() == 0);
     BOOST_CHECK(result[0] == 0);
 
-    result = BARSLAST(0);
-    BOOST_CHECK(result.size() == 1);
+    a.clear();
+    a.push_back(10);
+    a.push_back(20);
+    data = PRICELIST(a);
+    result = SUMBARS(data, 20);
+    BOOST_CHECK(result.size() == 2);
     BOOST_CHECK(result.discard() == 1);
     BOOST_CHECK(result[0] == Null<price_t>());
-#endif
+    BOOST_CHECK(result[1] == 0);
+
+    result = SUMBARS(data, 0);
+    BOOST_CHECK(result.size() == 2);
+    BOOST_CHECK(result.discard() == 0);
+    BOOST_CHECK(result[0] == 0);
+    BOOST_CHECK(result[1] == 0);
+
+    result = SUMBARS(data, 30);
+    BOOST_CHECK(result.size() == 2);
+    BOOST_CHECK(result.discard() == 1);
+    BOOST_CHECK(result[0] == Null<price_t>());
+    BOOST_CHECK(result[1] == 1);
+
+    result = SUMBARS(data, 40);
+    BOOST_CHECK(result.size() == 2);
+    BOOST_CHECK(result.discard() == 2);
+    BOOST_CHECK(result[0] == Null<price_t>());
+    BOOST_CHECK(result[1] == Null<price_t>());
 }
 
 
@@ -148,6 +172,7 @@ BOOST_AUTO_TEST_CASE( test_SUMBARS ) {
 
 /** @par 检测点 */
 BOOST_AUTO_TEST_CASE( test_SUMBARS_export ) {
+    #if 0
     StockManager& sm = StockManager::instance();
     string filename(sm.tmpdir());
     filename += "/SUMBARS.xml";
@@ -175,6 +200,7 @@ BOOST_AUTO_TEST_CASE( test_SUMBARS_export ) {
     for (size_t i = 0; i < x1.size(); ++i) {
         BOOST_CHECK_CLOSE(x1[i], x2[i], 0.00001);
     }
+    #endif
 }
 #endif /* #if HKU_SUPPORT_SERIALIZATION */
 
