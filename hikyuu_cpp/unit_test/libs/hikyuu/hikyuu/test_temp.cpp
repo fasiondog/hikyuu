12bef4d70e211705074a9fe8fb250161390e90d5
fasiondog <fasiondog@163.com>
2019-12-01 02:27:56 +0800

    test thread pool

--- hikyuu_cpp/unit_test/libs/hikyuu/hikyuu/test_temp.cpp
+++ hikyuu_cpp/unit_test/libs/hikyuu/hikyuu/test_temp.cpp
@@ -6,43 +6,7 @@
 #endif
 
 #include <hikyuu/StockManager.h>
-#include <hikyuu/data_driver/HistoryFinanceReader.h>
-#include <hikyuu/utilities/thread/ThreadPool.h>
 
 using namespace hku;
 
-int64 Fibon(int64 n) {
-    int64 num1 = 1;
-    int64 num2 = 1;
-    int64 tmp = 0;
-    int64 i = 0;
-    if (n < 3) {
-        return 1;
-    } else {
-        for (i = 0; i <= n - 3; i++) {
-            tmp = num1 + num2;
-            num1 = num2;
-            num2 = tmp;
-        }
-        return tmp;
-    }
-}
-
-double test_func() {
-    double x = 0;
-    for (int i = 0; i < 20000; i++) {
-        x += i;
-    }
-    return x;
-}
-
-BOOST_AUTO_TEST_CASE(test_temp) {
-    {
-        SPEND_TIME(test_temp);
-        ThreadPool tg;
-        for (int64 i = 0; i < 10; i++) {
-            tg.submit([=]() { std::cout << fmt::format("{}---------------------\n", i); });
-        }
-        tg.join();
-    }
-}
+BOOST_AUTO_TEST_CASE(test_temp) {}
--- hikyuu_cpp/unit_test/libs/hikyuu/utilities/test_ThreadPool.cpp
+++ hikyuu_cpp/unit_test/libs/hikyuu/utilities/test_ThreadPool.cpp
@@ -2,20 +2,21 @@
  * test_iniparser.cpp
  *
  *  Copyright (c) 2019 hikyuu.org
- * 
+ *
  *  Created on: 2010-5-26
  *      Author: fasiondog
  */
 
-
 #ifdef TEST_ALL_IN_ONE
-    #include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #else
-    #define BOOST_TEST_MODULE test_hikyuu_utilities
-    #include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE test_hikyuu_utilities
+#include <boost/test/unit_test.hpp>
 #endif
 
 #include <hikyuu/utilities/thread/ThreadPool.h>
+#include <hikyuu/utilities/SpendTimer.h>
+#include <fmt/format.h>
 
 using namespace hku;
 
@@ -26,15 +27,15 @@ using namespace hku;
  */
 
 /** @par 检测点 */
-BOOST_AUTO_TEST_CASE( test_ThreadPool ) {
-    ThreadPool tg;
-    for (int i = 0; i < 10; i++) {
-        tg.submit([&]() {
-            std::cout << i << ": --------------------" << std::endl;
-        });
+BOOST_AUTO_TEST_CASE(test_ThreadPool) {
+    {
+        SPEND_TIME(test_temp);
+        ThreadPool tg;
+        for (int i = 0; i < 10; i++) {
+            tg.submit([=]() { std::cout << fmt::format("{}---------------------\n", i); });
+        }
+        tg.join();
     }
-    //tg.wait_finish();
-    std::cout << "*********************" << std::endl;
 }
 
 /** @} */
