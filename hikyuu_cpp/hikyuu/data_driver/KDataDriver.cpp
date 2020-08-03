f13afe26ea471987eadff0d064271ff9b199da38
fasiondog <fasiondog@163.com>
2020-08-04 00:03:54 +0800

    修改打印提示

--- hikyuu_cpp/hikyuu/data_driver/KDataDriver.cpp
+++ hikyuu_cpp/hikyuu/data_driver/KDataDriver.cpp
@@ -65,30 +65,35 @@ bool KDataDriver::init(const Parameter& params) {
 }
 
 void KDataDriver::loadKData(const string& market, const string& code, KQuery::KType kType,
-                            size_t start_ix, size_t end_ix, KRecordListPtr out_buffer) {}
+                            size_t start_ix, size_t end_ix, KRecordListPtr out_buffer) {
+    HKU_INFO("The loadKData method has not been implemented! (KDataDriver: {})", m_name);
+}
 
 size_t KDataDriver::getCount(const string& market, const string& code, KQuery::KType kType) {
+    HKU_INFO("The getCount method has not been implemented! (KDataDriver: {})", m_name);
     return 0;
 }
 
 bool KDataDriver::getIndexRangeByDate(const string& market, const string& code, const KQuery& query,
                                       size_t& out_start, size_t& out_end) {
+    HKU_INFO("The getIndexRangeByDate method has not been implemented! (KDataDriver: {})", m_name);
     return false;
 }
 
 KRecord KDataDriver::getKRecord(const string& market, const string& code, size_t pos,
                                 KQuery::KType kType) {
+    HKU_INFO("The getKRecord method has not been implemented! (KDataDriver: {})", m_name);
     return Null<KRecord>();
 }
 
 TimeLineList KDataDriver::getTimeLineList(const string& market, const string& code,
                                           const KQuery& query) {
-    HKU_INFO("This driver not support time-line!");
+    HKU_INFO("The getTimeLineList method has not been implemented! (KDataDriver: {})", m_name);
     return TimeLineList();
 }
 
 TransList KDataDriver::getTransList(const string& market, const string& code, const KQuery& query) {
-    HKU_INFO("This driver not support transaction record!");
+    HKU_INFO("The getTransList method has not been implemented! (KDataDriver: {})", m_name);
     return TransList();
 }
 
