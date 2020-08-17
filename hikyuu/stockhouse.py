6057b0621d42e4895180246830daca457b627a57
fasiondog <fasiondog@163.com>
2020-08-18 00:45:17 +0800

    stock house (continue)

--- hikyuu/stockhouse.py
+++ hikyuu/stockhouse.py
@@ -9,6 +9,9 @@
 
 import os
 import sys
+import shutil
+import logging
+import importlib
 import git
 from configparser import ConfigParser
 
@@ -41,6 +44,7 @@ class HouseModel(Base):
     local = Column(String)  # 本地地址
     url = Column(String)  # git 仓库地址
     branch = Column(String)  # 远程仓库分支
+    status = Column(String)  # 导入状态 'success' | 'failed'
 
     __table_args__ = (UniqueConstraint('name'), )
 
@@ -56,13 +60,17 @@ class PartModel(Base):
     id = Column(Integer, Sequence('part_id_seq'), primary_key=True)
     house_id = Column(Integer)  #所属仓库标识
     part = Column(String)  # 部件类型
+    module_name = Column(String)  # 策略导入模块名
     name = Column(String)  # 策略名称
-    path = Column(String)  # 策略路径
+    author = Column(String)  # 策略作者
+    brief = Column(String)  # 策略概要描述
+    params = Column(String)  # 策略参数说明
 
 
 class StockHouse(object):
     """策略库管理"""
     def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
         usr_dir = os.path.expanduser('~')
 
         # 创建仓库数据库
@@ -85,6 +93,9 @@ class StockHouse(object):
 
         if not os.path.lexists(self.remote_cache_dir):
             os.makedirs(self.remote_cache_dir)
+
+        # 将远程仓库本地缓存地址加入系统路径
+        sys.path.append(self.remote_cache_dir)
 
         # 检查并下载 hikyuu 策略仓库
         hikyuu_house_path = self._session.query(HouseModel.local
@@ -112,7 +123,7 @@ class StockHouse(object):
         # 如果存在同名缓存目录，则强制删除
         local_dir = "{}/{}".format(self.remote_cache_dir, name)
         if os.path.lexists(local_dir):
-            os.removedirs(local_dir)
+            shutil.rmtree(local_dir)
 
         print('正在克隆至 "stockhouse/{}"'.format(name))
         try:
@@ -120,20 +131,72 @@ class StockHouse(object):
         except:
             raise RuntimeError("请检查网络是否正常或链接地址({})是否正确!".format(url))
 
-        # 导入仓库各部件策略信息
-        self.import_part_to_db(local_dir)
-
         # 更新仓库记录
         record = HouseModel(name=name, house_type='remote', url=url, branch=branch, local=local_dir)
         self._session.add(record)
         self._session.commit()
 
-    def import_part_to_db(self, local_dir):
-        for root, dirs, files in os.walk(local_dir):
-            for dir_name in dirs:
-                print(dir_name)
-            for file_name in files:
-                print(file_name)
+        # 导入仓库各部件策略信息
+        self.import_part_to_db(record)
+
+        # TODO 导入本地仓库部件信息
+
+    def import_part_to_db(self, house_model):
+        part_dict = {
+            'af': 'part/af',
+            'cn': 'part/cn',
+            'ev': 'part/ev',
+            'mm': 'part/mm',
+            'pg': 'part/pg',
+            'se': 'part/se',
+            'sg': 'part/sg',
+            'sp': 'part/sp',
+            'st_tp': 'part/st_tp',
+            'portfolio': 'portfolio',
+            'system': 'system',
+        }
+
+        # 检查仓库本地目录是否存在，不存在则给出告警信息并直接返回
+        local_dir = house_model.local
+        if not os.path.lexists(local_dir):
+            self.logger.warning(
+                'The {} house path ("{}") is not exists! Ignored this house!'.format(
+                    house_model.name, house_model.local
+                )
+            )
+            return
+
+        base_local = os.path.basename(local_dir)
+
+        for part, part_dir in part_dict.items():
+            path = "{}/{}".format(house_model.local, part_dir)
+            try:
+                with os.scandir(path) as it:
+                    for entry in it:
+                        if not entry.name.startswith('.') and entry.is_dir():
+                            module_name = '{}.part.{}.{}.part'.format(
+                                base_local, part, entry.name
+                            ) if part not in ('portfolio',
+                                              'system') else '{}.{}.part'.format(part, entry.name)
+                            print(sys.path)
+                            print(module_name)
+                            part_module = importlib.import_module(module_name)
+                            part_model = PartModel(
+                                house_id=house_model.id,
+                                part=part,
+                                module_name=module_name,
+                                name=part_module.name,
+                                author=part_module.author,
+                                brief=part_module.brief,
+                                params=str(part_module.params)
+                            )
+                            print(house_model.local, part, entry.name, module_name)
+            except FileNotFoundError:
+                continue
+        #for part_dir in part_dirs:
+        #    for root, dirs, files in os.walk("{}/{}".format(house_model.local, part_dir)):
+        #        for dir_name in dirs:
+        #            print(house_model.local, dir_name)
 
 
 if __name__ == "__main__":
