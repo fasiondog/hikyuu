0d3b6a04a6e920d1b76ace4ad5a6805440cde6f4
fasiondog <fasiondog@163.com>
2021-04-06 00:52:53 +0800

    update for hkuserver

---
+++ hikyuu_cpp/hikyuu_server/service/trade/model/StrategyFundsModel.h
@@ -0,0 +1,12 @@
+/*
+ *  Copyright(C) 2021 hikyuu.org
+ *
+ *  Create on: 2021-04-05
+ *     Author: fasiondog
+ */
+
+#pragma once
+
+#include <hikyuu/utilities/db_connect/TableMacro.h>
+
+namespace hku {}
