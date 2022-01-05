1666e7feb1a7deb8fc66388e63fba98a9579186a
fasiondog <fasiondog@sina.com>
2022-01-06 00:58:30 +0800

    add github 依赖包下载地址

--- hikyuu_extern_libs/packages/m/mysql/xmake.lua
+++ hikyuu_extern_libs/packages/m/mysql/xmake.lua
@@ -4,7 +4,8 @@ package("mysql")
     set_description("Open source relational database management system.")
 
     if is_plat("windows") then
-        add_urls("https://gitee.com/fasiondog/hikyuu/attach_files/935339/download/mysql-$(version)-win-x64.zip")
+        add_urls("https://gitee.com/fasiondog/hikyuu/attach_files/935339/download/mysql-$(version)-win-x64.zip",
+                 "https://github.com/fasiondog/hikyuu/releases/download/1.1.9/mysql-$(version)-win-x64.zip")
         add_versions("8.0.21", "de21694aa230a00b52b28babbce9bb150d990ba1f539edf8d193586dce3844ae")
     end
 
