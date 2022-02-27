a52e721a80285a11722a2a99b9b52efdb824acea
fasiondog <fasiondog@163.com>
2022-02-27 10:11:43 +0800

    更新 docs 下的示例

--- docs/examples/quick_crtsg.py
+++ docs/examples/quick_crtsg.py
@@ -7,33 +7,34 @@
 # History: 20160407, Added by fasiondog
 #===============================================================================
 
-from hikyuu.trade_sys.signal import crtSG
-from hikyuu.indicator import HHV, LLV, CLOSE, REF
+from hikyuu import *
+
 
 def TurtleSG(self):
-     n = self.getParam("n")
-     k = self.getTO()
-     c = CLOSE(k)
-     h = REF(HHV(c, n), 1) #前n日高点
-     L = REF(LLV(c, n), 1) #前n日低点
-     for i in range(h.discard, len(k)):
-         if (c[i] >= h[i]):
-             self._addBuySignal(k[i].datetime)
-         elif (c[i] <= L[i]):
-             self._addSellSignal(k[i].datetime)
+    n = self.get_param("n")
+    k = self.to
+    c = CLOSE(k)
+    h = REF(HHV(c, n), 1)  #前n日高点
+    L = REF(LLV(c, n), 1)  #前n日低点
+    for i in range(h.discard, len(k)):
+        if (c[i] >= h[i]):
+            self._add_buy_signal(k[i].datetime)
+        elif (c[i] <= L[i]):
+            self._add_sell_signal(k[i].datetime)
+
 
 if __name__ == "__main__":
     from examples_init import *
-    
+
     sg = crtSG(TurtleSG, {'n': 20}, 'TurtleSG')
-    s = getStock("sh000001")
-    k = s.getKData(Query(-500))
-    
+    s = get_stock("sh000001")
+    k = s.get_kdata(Query(-500))
+
     #只有设置交易对象时，才会开始实际计算
-    sg.setTO(k)
-    dates = k.getDatetimeList()
+    sg.to = k
+    dates = k.get_datetime_list()
     for d in dates:
-        if (sg.shouldBuy(d)):
+        if (sg.should_buy(d)):
             print("买入：%s" % d)
-        elif (sg.shouldSell(d)):
+        elif (sg.should_sell(d)):
             print("卖出: %s" % d)
