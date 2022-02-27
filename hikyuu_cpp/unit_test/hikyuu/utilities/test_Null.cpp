cdf84de65e0bfb9edbd028595f2bfb0ff7b3b580
fasiondog <fasiondog@sina.com>
2022-02-27 12:27:02 +0800

    fixed TradeManager::getPositon

---
+++ hikyuu_cpp/unit_test/hikyuu/utilities/test_Null.cpp
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2019 hikyuu.org
+ *
+ *  Created on: 2022-02-27
+ *      Author: fasiondog
+ */
+
+#include <doctest/doctest.h>
+#include <cstdint>
+#include <cmath>
+#include <hikyuu/utilities/Null.h>
+
+using namespace hku;
+
+TEST_CASE("test_Null_size_t") {
+    size_t null_size = Null<size_t>();
+    CHECK_EQ(std::numeric_limits<std::size_t>::max(), null_size);
+
+    CHECK_UNARY(std::isnan(Null<double>()));
+}
