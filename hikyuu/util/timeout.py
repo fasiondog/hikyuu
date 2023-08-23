995fd9ac16428ffc93ce24c4b685497ad43c50c6
fasiondog <fasiondog@163.com>
2023-08-23 19:50:24 +0800

    fixed timeout on linux 改为和windows一样的机制

--- hikyuu/util/timeout.py
+++ hikyuu/util/timeout.py
@@ -5,7 +5,6 @@
 import ctypes
 import threading
 import functools
-import sys
 
 
 class ThreadKiller(threading.Thread):
@@ -47,54 +46,37 @@ def timeout(sec, raise_sec=1):
         def wrapped_func(*args, **kwargs):
             err_msg = f'Function {func.__name__} timed out after {sec} seconds'
 
-            if sys.platform != 'win32':
-                import signal
+            class FuncTimeoutError(TimeoutError):
+                def __init__(self):
+                    TimeoutError.__init__(self, err_msg)
 
-                def _handle_timeout(signum, frame):
-                    raise TimeoutError(err_msg)
+            result, exception = [], []
 
-                signal.signal(signal.SIGALRM, _handle_timeout)
-                signal.alarm(sec)
+            def run_func():
                 try:
-                    result = func(*args, **kwargs)
-                finally:
-                    signal.alarm(0)
-                return result
-
-            else:
-
-                class FuncTimeoutError(TimeoutError):
-                    def __init__(self):
-                        TimeoutError.__init__(self, err_msg)
-
-                result, exception = [], []
-
-                def run_func():
-                    try:
-                        res = func(*args, **kwargs)
-                    except FuncTimeoutError:
-                        pass
-                    except Exception as e:
-                        exception.append(e)
-                    else:
-                        result.append(res)
-
-                # typically, a python thread cannot be terminated, use TerminableThread instead
-                thread = TerminableThread(target=run_func, daemon=True)
-                thread.start()
-                thread.join(timeout=sec)
-
-                if thread.is_alive():
-                    # a timeout thread keeps alive after join method, terminate and raise TimeoutError
-                    exc = type('TimeoutError', FuncTimeoutError.__bases__, dict(FuncTimeoutError.__dict__))
-                    thread.terminate(exception_cls=FuncTimeoutError, repeat_sec=raise_sec)
-                    raise TimeoutError(err_msg)
-                elif exception:
-                    # if exception occurs during the thread running, raise it
-                    raise exception[0]
+                    res = func(*args, **kwargs)
+                except FuncTimeoutError:
+                    pass
+                except Exception as e:
+                    exception.append(e)
                 else:
-                    # if the thread successfully finished, return its results
-                    return result[0]
+                    result.append(res)
+
+            # typically, a python thread cannot be terminated, use TerminableThread instead
+            thread = TerminableThread(target=run_func, daemon=True)
+            thread.start()
+            thread.join(timeout=sec)
+            if thread.is_alive():
+                # a timeout thread keeps alive after join method, terminate and raise TimeoutError
+                exc = type('TimeoutError', FuncTimeoutError.__bases__, dict(FuncTimeoutError.__dict__))
+                thread.terminate(exception_cls=FuncTimeoutError, repeat_sec=raise_sec)
+                raise TimeoutError(err_msg)
+            elif exception:
+                # if exception occurs during the thread running, raise it
+                raise exception[0]
+            else:
+                # if the thread successfully finished, return its results
+                return result[0]
 
         return wrapped_func
 
