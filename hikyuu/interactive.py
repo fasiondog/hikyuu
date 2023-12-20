482734c00b15bfd8d79d82870f8a3cf2285991d4
fasiondog <fasiondog@163.com>
2023-12-21 01:20:37 +0800

    屏蔽interactive里sina的实时数据获取

--- hikyuu/interactive.py
+++ hikyuu/interactive.py
@@ -364,9 +364,11 @@ def realtimePartUpdate_from_qq(queryStr):
 
 def realtime_update_from_sina_qq(source):
     if source == 'sina':
-        queryStr = "http://hq.sinajs.cn/list="
-        update_func = realtimePartUpdate_from_sina
-        max_size = 140
+        hku_error("sina已不支持获取实时数据")
+        return
+        # queryStr = "http://hq.sinajs.cn/list="
+        # update_func = realtimePartUpdate_from_sina
+        # max_size = 140
     elif source == 'qq':
         queryStr = "http://qt.gtimg.cn/q="
         update_func = realtimePartUpdate_from_qq
