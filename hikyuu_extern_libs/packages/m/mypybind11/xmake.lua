964811c985dd0508e6361b81246867107106b24e
fasiondog <fasiondog@163.com>
2024-01-01 19:57:56 +0800

    update

--- hikyuu_extern_libs/packages/m/mypybind11/xmake.lua
+++ hikyuu_extern_libs/packages/m/mypybind11/xmake.lua
@@ -1,6 +1,7 @@
 package("mypybind11")
     set_base("pybind11")
 
+    add_deps("cmake", "python 3.x")
     on_install("windows", "macosx", "linux", "cross", function (package)
         import("package.tools.cmake").install(package, {"-DPYBIND11_TEST=OFF"})
     end)
