94f79eba57a7cc76b69c0a3c11b5e2ab1d1fe755
martin <yang.ruoqi@outlook.com>
2024-01-29 19:16:03 +0800

    use python -c to get python version

--- hikyuu_pywrap/xmake.lua
+++ hikyuu_pywrap/xmake.lua
@@ -83,13 +83,13 @@ target("core")
 
         local dst_dir = "$(projectdir)/hikyuu/cpp/"
         local dst_obj = dst_dir .. "core.so"
+
+        -- need xmake 445e43b40846b29b9abb1293b32b27b7104f54fa
         if not is_plat("cross") then
-            import("lib.detect.find_tool")
-            local python = assert(find_tool("python", {version = true, force = true}), "python not found, please install it first! note: python version must > 3.0")
-            local tmp = string.split(python.version, "%.")
-            dst_obj = dst_dir .. "core" .. tmp[1] .. tmp[2]
+          local stmt = [[python -c 'import sys; v = sys.version_info; print(str(v.major)+str(v.minor))']]
+          local python_version = os.iorun(stmt):trim()
+          dst_obj = dst_dir .. "core" ..  python_version
         end
-        -- print(dst_obj)
 
         if is_plat("windows") then
             os.cp(target:targetdir() .. '/core.pyd', dst_obj .. ".pyd")
