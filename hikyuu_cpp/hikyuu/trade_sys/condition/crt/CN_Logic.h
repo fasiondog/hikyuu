cbd59ccb1c354375734cd99e1eddb5afd30767b6
fasiondog <fasiondog@163.com>
2024-02-23 17:05:02 +0800

    add Condition operator /

--- hikyuu_cpp/hikyuu/trade_sys/condition/crt/CN_Logic.h
+++ hikyuu_cpp/hikyuu/trade_sys/condition/crt/CN_Logic.h
@@ -30,5 +30,6 @@ HKU_API ConditionPtr operator|(const ConditionPtr& cond1, const ConditionPtr& co
 HKU_API ConditionPtr operator+(const ConditionPtr& cond1, const ConditionPtr& cond2);
 HKU_API ConditionPtr operator-(const ConditionPtr& cond1, const ConditionPtr& cond2);
 HKU_API ConditionPtr operator*(const ConditionPtr& cond1, const ConditionPtr& cond2);
+HKU_API ConditionPtr operator/(const ConditionPtr& cond1, const ConditionPtr& cond2);
 
 }  // namespace hku
--- hikyuu_pywrap/trade_sys/_Condition.cpp
+++ hikyuu_pywrap/trade_sys/_Condition.cpp
@@ -121,6 +121,9 @@ void export_Condition(py::module& m) {
       .def("__mul__",
            [](const ConditionPtr& self, const ConditionPtr& other) { return self * other; })
 
+      .def("__truediv__",
+           [](const ConditionPtr& self, const ConditionPtr& other) { return self / other; })
+
         DEF_PICKLE(ConditionPtr);
 
     m.def("CN_OPLine", CN_OPLine, R"(CN_OPLine(ind)
