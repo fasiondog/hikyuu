97f86f0b51e0a964e2148426457482e9f521ab8c
fasiondog <fasiondog@163.com>
2024-02-28 00:54:55 +0800

    add some testcases

--- hikyuu_cpp/unit_test/hikyuu/indicator/test_ATR.cpp
+++ hikyuu_cpp/unit_test/hikyuu/indicator/test_ATR.cpp
@@ -10,57 +10,56 @@
 #include "../test_config.h"
 #include <fstream>
 #include <hikyuu/StockManager.h>
-#include <hikyuu/indicator/crt/ACOS.h>
+#include <hikyuu/indicator/crt/ATR.h>
 #include <hikyuu/indicator/crt/KDATA.h>
 #include <hikyuu/indicator/crt/PRICELIST.h>
 
 using namespace hku;
 
 /**
- * @defgroup test_indicator_ACOS test_indicator_ACOS
+ * @defgroup test_indicator_ATR test_indicator_ATR
  * @ingroup test_hikyuu_indicator_suite
  * @{
  */
 
 /** @par 检测点 */
-TEST_CASE("test_ACOS") {
+TEST_CASE("test_ATR") {
     Indicator result;
 
     PriceList a;
     for (int i = 0; i < 10; ++i) {
-        a.push_back(i / 10);
+        a.push_back(i / 2.);
     }
+
+    vector<Indicator::value_t> expect = {0.,      0.333333, 0.77778, 1.25926, 1.75309,
+                                         2.25103, 2.75034,  3.25011, 3.75004, 4.25001};
 
     Indicator data = PRICELIST(a);
 
-    result = ACOS(data);
-    CHECK_EQ(result.name(), "ACOS");
+    result = ATR(data, 2);
+    CHECK_EQ(result.name(), "ATR");
     CHECK_EQ(result.discard(), 0);
-    for (int i = 0; i < 10; ++i) {
-        CHECK_EQ(result[i], std::acos(data[i]));
+    CHECK_EQ(result.size(), data.size());
+    for (int i = 0, len = data.size(); i < len; ++i) {
+        CHECK_EQ(result[i], doctest::Approx(expect[i]));
     }
-
-    result = ACOS(-0.1);
-    CHECK_EQ(result.size(), 1);
-    CHECK_EQ(result.discard(), 0);
-    CHECK_EQ(result[0], doctest::Approx(std::acos(-0.1)));
 }
 
 //-----------------------------------------------------------------------------
 // benchmark
 //-----------------------------------------------------------------------------
 #if ENABLE_BENCHMARK_TEST
-TEST_CASE("test_ACOS_benchmark") {
+TEST_CASE("test_ATR_benchmark") {
     Stock stock = getStock("sh000001");
     KData kdata = stock.getKData(KQuery(0));
     Indicator c = kdata.close();
     int cycle = 1000;  // 测试循环次数
 
     {
-        BENCHMARK_TIME_MSG(test_ACOS_benchmark, cycle, fmt::format("data len: {}", c.size()));
+        BENCHMARK_TIME_MSG(test_ATR_benchmark, cycle, fmt::format("data len: {}", c.size()));
         SPEND_TIME_CONTROL(false);
         for (int i = 0; i < cycle; i++) {
-            Indicator ind = ACOS();
+            Indicator ind = ATR();
             Indicator result = ind(c);
         }
     }
@@ -73,14 +72,14 @@ TEST_CASE("test_ACOS_benchmark") {
 #if HKU_SUPPORT_SERIALIZATION
 
 /** @par 检测点 */
-TEST_CASE("test_ACOS_export") {
+TEST_CASE("test_ATR_export") {
     StockManager& sm = StockManager::instance();
     string filename(sm.tmpdir());
-    filename += "/ACOS.xml";
+    filename += "/ATR.xml";
 
     Stock stock = sm.getStock("sh000001");
     KData kdata = stock.getKData(KQuery(-20));
-    Indicator x1 = ACOS(CLOSE(kdata));
+    Indicator x1 = ATR(CLOSE(kdata));
     {
         std::ofstream ofs(filename);
         boost::archive::xml_oarchive oa(ofs);
@@ -94,16 +93,12 @@ TEST_CASE("test_ACOS_export") {
         ia >> BOOST_SERIALIZATION_NVP(x2);
     }
 
-    CHECK_EQ(x2.name(), "ACOS");
+    CHECK_EQ(x2.name(), "ATR");
     CHECK_EQ(x1.size(), x2.size());
     CHECK_EQ(x1.discard(), x2.discard());
     CHECK_EQ(x1.getResultNumber(), x2.getResultNumber());
     for (size_t i = x1.discard(); i < x1.size(); ++i) {
-        if (std::isnan(x1[i])) {
-            CHECK_UNARY(std::isnan(x2[i]));
-        } else {
-            CHECK_EQ(x1[i], doctest::Approx(x2[i]));
-        }
+        CHECK_EQ(x1[i], doctest::Approx(x2[i]));
     }
 }
 #endif /* #if HKU_SUPPORT_SERIALIZATION */
