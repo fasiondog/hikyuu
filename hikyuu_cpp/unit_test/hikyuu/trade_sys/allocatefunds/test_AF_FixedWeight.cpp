2834ba913734e8a6432d4e0005714b636de031ae
fasiondog <fasiondog@163.com>
2024-03-27 02:29:32 +0800

    add test case

--- hikyuu_cpp/unit_test/hikyuu/trade_sys/allocatefunds/test_AF_FixedWeight.cpp
+++ hikyuu_cpp/unit_test/hikyuu/trade_sys/allocatefunds/test_AF_FixedWeight.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "../../test_config.h"
+#include "hikyuu/trade_sys/allocatefunds/crt/AF_FixedWeight.h"
 
 using namespace hku;
 
@@ -16,6 +17,15 @@ using namespace hku;
  */
 
 /** @par 检测点 */
-TEST_CASE("test_AF_FixedWeight") {}
+TEST_CASE("test_AF_FixedWeight") {
+    /** @arg 非法参数 */
+    CHECK_THROWS_AS(AF_FixedWeight(0.0), std::exception);
+    CHECK_THROWS_AS(AF_FixedWeight(-0.1), std::exception);
+    CHECK_THROWS_AS(AF_FixedWeight(1.001), std::exception);
+
+    /** @arg 尝试变更非法的公共参数 */
+    auto af = AF_FixedWeight(0.1);
+    CHECK_THROWS_AS(af->setParam<bool>("auto_adjust_weight", true), std::exception);
+}
 
 /** @} */
