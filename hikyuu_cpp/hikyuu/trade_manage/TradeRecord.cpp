0723e115c5ab5193dc5a0560aaeaff9f41fe4a78
fasiondog <fasiondog@163.com>
2024-04-07 02:36:39 +0800

    调整 PF/TradeRecord 打印

--- hikyuu_cpp/hikyuu/trade_manage/TradeRecord.cpp
+++ hikyuu_cpp/hikyuu/trade_manage/TradeRecord.cpp
@@ -125,18 +125,10 @@ string TradeRecord::toString() const {
         name = stock.name();
     }
 
-#if HKU_OS_WINDOWS
-    return fmt::format("Trade({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})", datetime,
-                       market_code, pythonInInteractive() ? name : UTF8ToGB(name),
-                       getBusinessName(business), planPrice, realPrice, goalPrice, number,
-                       cost.commission, cost.stamptax, cost.transferfee, cost.others,
-                       getSystemPartName(from));
-#else
     return fmt::format("Trade({}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})", datetime,
                        market_code, name, getBusinessName(business), planPrice, realPrice,
                        goalPrice, number, cost.commission, cost.stamptax, cost.transferfee,
                        cost.others, getSystemPartName(from));
-#endif
 }
 
 bool TradeRecord::isNull() const {
