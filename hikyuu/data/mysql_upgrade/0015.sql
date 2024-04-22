813eea963bac46f3ca421da888f6b090832ad994
fasiondog <fasiondog@163.com>
2024-04-22 20:42:35 +0800

    updaate

--- hikyuu/data/mysql_upgrade/0015.sql
+++ hikyuu/data/mysql_upgrade/0015.sql
@@ -1,2 +1,3 @@
+ALTER TABLE `hku_base`.`coderuletype` modify `id` int(11)  auto_increment;
 INSERT INTO `hku_base`.`coderuletype` (`marketid`,`codepre`,`type`,`description`) VALUES (3,'92',11,'北证A股');
 UPDATE `hku_base`.`version` set `version` = 15;
