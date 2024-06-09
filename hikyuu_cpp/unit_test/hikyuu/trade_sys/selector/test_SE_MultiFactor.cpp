e008cea5d859600c5e48193e719c06356c67e33f
fasiondog <fasiondog@163.com>
2024-06-10 01:20:51 +0800

    补充测试，fixed System 序列化存储

--- hikyuu_cpp/unit_test/hikyuu/trade_sys/selector/test_SE_MultiFactor.cpp
+++ hikyuu_cpp/unit_test/hikyuu/trade_sys/selector/test_SE_MultiFactor.cpp
@@ -32,7 +32,7 @@ TEST_CASE("test_SE_MultiFactor") {
     StockList stks{sm["sh600004"], sm["sh600005"], sm["sz000001"], sm["sz000002"]};
     Stock ref_stk = sm["sh000001"];
     KQuery query = KQueryByDate(Datetime(20110712), Datetime(20111206));
-    IndicatorList src_inds{MA(CLOSE()), EMA(CLOSE())};
+    IndicatorList src_inds{MA(CLOSE())};
 
     auto sys = SYS_Simple(crtTM(), MM_Nothing());
     sys->setSG(SG_Cycle());
@@ -48,12 +48,28 @@ TEST_CASE("test_SE_MultiFactor") {
     CHECK_THROWS(SE_MultiFactor(IndicatorList{}));
 
     /** @arg 默认参数 */
-    ret = SE_MultiFactor(src_inds, 10, 5, 120, ref_stk);
+    ret = SE_MultiFactor(src_inds, 10, 5, 120, ref_stk, "MF_ICIRWeight");
     ret->addStockList(stks, sys);
     auto proto_list = ret->getProtoSystemList();
     ret->calculate(proto_list, query);
     auto sw_list = ret->getSelected(Datetime(20110712));
     CHECK_EQ(sw_list.size(), 4);
+
+#if 0
+    ret = SE_MultiFactor(src_inds, 10, 5, 120, ref_stk, "MF_EqualWeight");
+    ret->addStockList(stks, sys);
+    proto_list = ret->getProtoSystemList();
+    ret->calculate(proto_list, query);
+    sw_list = ret->getSelected(Datetime(20110712));
+    CHECK_EQ(sw_list.size(), 4);
+
+    ret = SE_MultiFactor(src_inds, 10, 5, 120, ref_stk, "MF_ICWeight");
+    ret->addStockList(stks, sys);
+    proto_list = ret->getProtoSystemList();
+    ret->calculate(proto_list, query);
+    sw_list = ret->getSelected(Datetime(20110712));
+    CHECK_EQ(sw_list.size(), 4);
+#endif
 
     /** @arg topn = 2 */
     ret = SE_MultiFactor(src_inds, 2, 5, 120, ref_stk);
@@ -74,7 +90,40 @@ TEST_CASE("test_SE_MultiFactor") {
 #if HKU_SUPPORT_SERIALIZATION
 
 /** @par 检测点 */
-TEST_CASE("test_SE_MultiFactor_export") {}
+TEST_CASE("test_SE_MultiFactor_export") {
+    StockManager& sm = StockManager::instance();
+    string filename(fmt::format("{}/SE_MultiFactor.xml", sm.tmpdir()));
+
+    StockList stks{sm["sh600004"], sm["sh600005"], sm["sz000001"], sm["sz000002"]};
+    Stock ref_stk = sm["sh000001"];
+    KQuery query = KQueryByDate(Datetime(20110712), Datetime(20111206));
+    IndicatorList src_inds{MA(CLOSE())};
+
+    auto sys = SYS_Simple(crtTM(), MM_Nothing());
+    sys->setSG(SG_Cycle());
+    sys->setParam<bool>("buy_delay", false);
+
+    auto x1 = SE_MultiFactor(src_inds, 10, 5, 120, ref_stk);
+    x1->addStockList(stks, sys);
+
+    {
+        std::ofstream ofs(filename);
+        boost::archive::xml_oarchive oa(ofs);
+        oa << BOOST_SERIALIZATION_NVP(x1);
+    }
+
+    SelectorPtr x2;
+    {
+        std::ifstream ifs(filename);
+        boost::archive::xml_iarchive ia(ifs);
+        ia >> BOOST_SERIALIZATION_NVP(x2);
+    }
+
+    auto proto_list = x2->getProtoSystemList();
+    x2->calculate(proto_list, query);
+    auto sw_list = x2->getSelected(Datetime(20110712));
+    CHECK_EQ(sw_list.size(), 4);
+}
 #endif /* #if HKU_SUPPORT_SERIALIZATION */
 
 /** @} */
