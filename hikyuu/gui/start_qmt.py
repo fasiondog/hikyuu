8021a1b808ff7cc8712121ef5b5109120532bed3
KongDong <fasiondog@163.com>
2024-09-06 12:22:14 +0800

    update start_qmt.py

--- hikyuu/gui/start_qmt.py
+++ hikyuu/gui/start_qmt.py
@@ -19,7 +19,83 @@ def callback(datas):
 
 
 if __name__ == "__main__":
-    from hikyuu.interactive import *
+    import os
+    import configparser
+
+    from hikyuu.data.hku_config_template import generate_default_config
+    from hikyuu import *
+
+    config_file = os.path.expanduser('~') + "/.hikyuu/hikyuu.ini"
+    if not os.path.exists(config_file):
+        # 创建默认配置
+        hku_info("创建默认配置文件")
+        generate_default_config()
+
+    ini = configparser.ConfigParser()
+    ini.read(config_file, encoding='utf-8')
+    hku_param = Parameter()
+    hku_param["tmpdir"] = ini.get('hikyuu', 'tmpdir')
+    hku_param["datadir"] = ini.get('hikyuu', 'datadir')
+    if ini.has_option('hikyuu', 'quotation_server'):
+        hku_param["quotation_server"] = ini['hikyuu']['quotation_server']
+
+    # 不加载历史财务信息及权息数据
+    hku_param["load_history_finance"] = False
+    hku_param["load_stock_weight"] = False
+
+    base_param = Parameter()
+    base_info_config = ini.options('baseinfo')
+    for p in base_info_config:
+        base_param[p] = ini.get('baseinfo', p)
+
+    block_param = Parameter()
+    block_config = ini.options('block')
+    for p in block_config:
+        block_param[p] = ini.get('block', p)
+
+    # 不使用配置文件中的预加载参数
+    preload_param = Parameter()
+    if p in Query.get_all_ktype():
+        preload_param[p] = True
+        minute = Query.get_ktype_in_min()
+        if minute >= 240:
+            preload_param[f'{p.lower()}_max'] = 1
+        else:
+            preload_param[f'{p.lower()}_max'] = 240 // minute
+
+    kdata_param = Parameter()
+    kdata_config = ini.options('kdata')
+    for p in kdata_config:
+        if p == "convert":
+            kdata_param[p] = ini.getboolean('kdata', p)
+            continue
+        kdata_param[p] = ini.get('kdata', p)
+
+    context = StrategyContext(["all"])
+    if 'HKU_STOCK_LIST' in os.environ:
+        context.stock_list = os.environ['HKU_STOCK_LIST'].split(";")
+    if 'HKU_KTYPE_LIST' in os.environ:
+        context.ktype_list = os.environ['HKU_KTYPE_LIST'].split(";")
+    if 'HKU_LOAD_HISTORY_FINANCE' in os.environ:
+        load_str = os.environ['HKU_LOAD_HISTORY_FINANCE'].upper()
+        load_finance = load_str in ("1", "TRUE")
+        hku_param.set("load_history_finance", load_finance)
+    if 'HKU_LOAD_STOCK_WEIGHT' in os.environ:
+        load_str = os.environ['HKU_LOAD_STOCK_WEIGHT'].upper()
+        load_stk_weight = load_str in ("1", "TRUE")
+        hku_param.set("load_stock_weight", load_stk_weight)
+
+    sm.init(base_param, block_param, kdata_param, preload_param, hku_param, context)
+    # set_log_level(LOG_LEVEL.INFO)
+
+    start_spot = False
+    if 'HKU_START_SPOT' in os.environ:
+        spot_str = os.environ['HKU_START_SPOT'].upper()
+        start_spot = spot_str in ('1', 'TRUE')
+    spot_worker_num = 1
+    if 'HKU_SPOT_WORKER_NUM' in os.environ:
+        spot_worker_num = int(os.environ['HKU_SPOT_WORKER_NUM'])
+
     from xtquant import xtdata
 
     code_list = [f'{s.code}.{s.market}' for s in sm if s.valid]
