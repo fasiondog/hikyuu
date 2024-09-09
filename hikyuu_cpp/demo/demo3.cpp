9388d8e10f5d12d52bda8cb1b53373d74b86378f
fasiondog <fasiondog@163.com>
2024-09-09 09:05:02 +0800

    update demo3 spot worker num

--- hikyuu_cpp/demo/demo3.cpp
+++ hikyuu_cpp/demo/demo3.cpp
@@ -85,7 +85,7 @@ int main(int argc, char* argv[]) {
                                       hkuParam);
 
         // 启动行情接收
-        startSpotAgent(true);
+        startSpotAgent(true, 3);
 
         server.setAddr("tcp://0.0.0.0:9201");
 
