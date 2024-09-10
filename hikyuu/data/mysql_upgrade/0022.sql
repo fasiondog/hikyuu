6f6597d917470270f447c2478b39061c9c9fe305
fasiondog <fasiondog@163.com>
2024-09-10 18:10:57 +0800

    fixded mysql_upgrade/0022.sql

--- hikyuu/data/mysql_upgrade/0022.sql
+++ hikyuu/data/mysql_upgrade/0022.sql
@@ -1,4 +1,4 @@
-DELETE FROM `hku_base`.`stkWeight`;
+DELETE FROM `hku_base`.`stkweight`;
 UPDATE `hku_base`.`coderuletype` SET `codepre`=51 WHERE `codepre`=510 AND `marketid`=1;
 UPDATE `hku_base`.`coderuletype` SET `codepre`=50 WHERE `codepre`=500 AND `marketid`=1;
 UPDATE `hku_base`.`coderuletype` SET `codepre`=20 WHERE `codepre`=200 AND `marketid`=2;
