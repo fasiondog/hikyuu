8c36c7c495b780da623dc126d37b5fde13bbfe4c
fasiondog <fasiondog@163.com>
2024-10-09 11:55:59 +0800

    fixed TABLE_NO_AUTOID_BIND20

--- hikyuu_cpp/hikyuu/utilities/db_connect/TableMacro.h
+++ hikyuu_cpp/hikyuu/utilities/db_connect/TableMacro.h
@@ -2571,7 +2571,7 @@ public:                                                                         
     }                                                                                              \
     void load(const SQLStatementPtr& st) {                                                         \
         st->getColumn(0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16,    \
-                      f17, f18, f20);                                                              \
+                      f17, f18, f19, f20);                                                         \
     }
 
 }  // namespace hku
