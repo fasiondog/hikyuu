66696c4cddcb406e08650fd8f9b35cbb387f9223
KongDong <fasiondog@163.com>
2024-11-03 03:01:54 +0800

    2.2.3 for linux 滚动系统崩溃

--- xmake.lua
+++ xmake.lua
@@ -6,7 +6,7 @@ set_project("hikyuu")
 add_rules("mode.debug", "mode.release")
 
 -- version
-set_version("2.2.2", {build = "%Y%m%d%H%M"})
+set_version("2.2.3", {build = "%Y%m%d%H%M"})
 
 set_warnings("all")
 
