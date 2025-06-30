7c321efa5ec34c80b061a2664560d67ca567c83f
fasiondog <fasiondog@163.com>
2025-07-01 03:30:14 +0800

    fix(data): 修复 MySQLKDataDriver 中获取交易记录列表的问题
    - 在 getColumn 方法调用中添加 direct 参数，确保正确获取交易方向
    - 这个修改解决了之前无法正确获取交易记录中方向信息的问题

--- hikyuu_cpp/hikyuu/data_driver/kdata/mysql/MySQLKDataDriver.cpp
+++ hikyuu_cpp/hikyuu/data_driver/kdata/mysql/MySQLKDataDriver.cpp
@@ -359,7 +359,7 @@ TransList MySQLKDataDriver::_getTransListByIndex(const string& market, const str
                     uint64_t date = 0;
                     double price = 0.0, vol = 0.0;
                     int direct = 0;
-                    st->getColumn(0, date, price, vol);
+                    st->getColumn(0, date, price, vol, direct);
                     result.emplace_back(Datetime(date), price, vol,
                                         static_cast<TransRecord::DIRECT>(direct));
                 } catch (const std::exception& e) {
