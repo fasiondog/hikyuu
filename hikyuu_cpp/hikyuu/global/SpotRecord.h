b590ca86cbe36dca43c6e1af2350281ab6126456
fasiondog <fasiondog@163.com>
2025-07-13 21:24:53 +0800

    - 修改 pytdx 实时数据接口返回值结构 - 更新 SpotRecord 结构体注释

--- hikyuu_cpp/hikyuu/global/SpotRecord.h
+++ hikyuu_cpp/hikyuu/global/SpotRecord.h
@@ -16,21 +16,21 @@ namespace hku {
  * @ingroup Agent
  */
 struct HKU_API SpotRecord {
-    string market;            ///< 市场标识
-    string code;              ///< 证券代码
-    string name;              ///< 证券名称
-    Datetime datetime;        ///< 数据时间
-    price_t yesterday_close;  ///< 昨日收盘价
-    price_t open;             ///< 开盘价
-    price_t high;             ///< 最高价
-    price_t low;              ///< 最低价
-    price_t close;            ///< 收盘价
-    price_t amount;           ///< 成交金额 （千元）
-    price_t volume;           ///< 成交量（手）
-    vector<double> bid;
-    vector<double> bid_amount;
-    vector<double> ask;
-    vector<double> ask_amount;
+    string market;              ///< 市场标识
+    string code;                ///< 证券代码
+    string name;                ///< 证券名称
+    Datetime datetime;          ///< 数据时间
+    price_t yesterday_close;    ///< 昨日收盘价
+    price_t open;               ///< 开盘价
+    price_t high;               ///< 最高价
+    price_t low;                ///< 最低价
+    price_t close;              ///< 收盘价
+    price_t amount;             ///< 成交金额 （千元）
+    price_t volume;             ///< 成交量（手）
+    vector<double> bid;         ///< 委买价
+    vector<double> bid_amount;  ///< 委买量
+    vector<double> ask;         ///< 委卖价
+    vector<double> ask_amount;  ///< 委卖量
 };
 
 }  // namespace hku
