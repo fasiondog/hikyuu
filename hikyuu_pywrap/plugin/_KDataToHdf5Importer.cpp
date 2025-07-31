cfb6dd236c2e6e13496134997f456cd1a850ce9d
fasiondog <fasiondog@163.com>
2025-07-31 20:51:23 +0800

    feat(hikyuu_pywrap): 为 KDataToHdf5Importer 的 set_config 方法添加默认参数

--- hikyuu_pywrap/plugin/_KDataToHdf5Importer.cpp
+++ hikyuu_pywrap/plugin/_KDataToHdf5Importer.cpp
@@ -14,7 +14,10 @@ using namespace hku;
 void export_plugin_KDataToHdf5Importer(py::module& m) {
     py::class_<KDataToHdf5Importer>(m, "KDataToHdf5Importer", "K线数据导入器")
       .def(py::init<>())
-      .def("set_config", &KDataToHdf5Importer::setConfig, "设置数据保存路径和数据源列表")
+      .def("set_config", &KDataToHdf5Importer::setConfig, py::arg("datapath"),
+           py::arg("markets") = vector<string>({"SH", "SZ", "BJ"}),
+           py::arg("ktypes") = vector<string>({"DAY", "MIN", "MIN5", "TIMELINE", "TRANSDATA"}),
+           "设置数据保存路径和数据源列表")
       .def("get_last_datetime", &KDataToHdf5Importer::getLastDatetime,
            "获取指定市场指定证券最后K线时间")
       .def("add_krecord_list", &KDataToHdf5Importer::addKRecordList, "添加K线数据")
