87299d3c385931eb6472041538076ba8ddc9e8b2
fasiondog <fasiondog@163.com>
2025-08-07 01:08:10 +0800

    feat(plugin): 增加按 bar 数量合成的扩展 K 线类型

--- hikyuu_cpp/hikyuu/plugin/hkuextra.h
+++ hikyuu_cpp/hikyuu/plugin/hkuextra.h
@@ -12,7 +12,7 @@
 namespace hku {
 
 /**
- * @brief 注册扩展 K 线类型
+ * @brief 注册扩展 K 线类型, 按时间合成
  * @param ktype 扩展 K 线类型
  * @param basetype 对应的基础 K 线类型
  * @param minutes 每根K线所包含的分钟数
@@ -21,6 +21,19 @@ namespace hku {
 void HKU_API registerKTypeExtra(const string& ktype, const string& basetype, int32_t minutes,
                                 std::function<Datetime(const Datetime&)> getPhaseEnd);
 
+/**
+ * 注册K线扩展类型，按 bar 数量合成
+ * @param ktype 扩展 K 线类型
+ * @param basetype 对应的基础 K 线类型
+ * @param nbars 每根K线所包含的基础K线数
+ */
+void HKU_API registerKTypeExtra(const string& ktype, const string& basetype, int32_t nbars);
+
+/**
+ * 释放扩展K线
+ * @note 某些情况下，注册扩展K线时，使用了 python 定义的周期结束日期转换函数，退出时可能引起 python
+ * GIL 报错，需手工提前释放扩展K线
+ */
 void HKU_API releaseKExtra();
 
 //-------------------------------
--- hikyuu_pywrap/plugin/_hkuextra.cpp
+++ hikyuu_pywrap/plugin/_hkuextra.cpp
@@ -13,6 +13,24 @@ using namespace hku;
 namespace py = pybind11;
 
 void export_hkuextra(py::module& m) {
-    m.def("register_ktype_extra", &registerKTypeExtra);
-    m.def("release_ktype_extra", &releaseKExtra);
+    m.def("register_ktype_extra",
+          py::overload_cast<const string&, const string&, int32_t,
+                            std::function<Datetime(const Datetime&)>>(registerKTypeExtra),
+          py::arg("ktype"), py::arg("basetype"), py::arg("minutes"), py::arg("get_phase_end"));
+    m.def("register_ktype_extra",
+          py::overload_cast<const string&, const string&, int32_t>(registerKTypeExtra),
+          py::arg("ktype"), py::arg("basetype"), py::arg("nbars"),
+          R"(register_ktype_extra(ktype, basetype, nbars|minutes[, get_phase_end])
+          
+    注册扩展K线类型, 实现自定义动态周期K线
+    
+    :param str ktype: 扩展K线类型名称
+    :param str basetype: 基础K线类型名称
+    :param int nbars|minutes: 基础K线类型对应的基础周期数或分钟数
+    :param func get_phase_end: 日期转换函数，参数为日期，返回该日期对应的周期结束日期
+    :return: None)");
+
+    m.def("release_ktype_extra", &releaseKExtra, R"(release_ktype_extra()
+        
+    释放扩展K线类型)");
 }
