26fddfc4b088419f50979b4c7f6000097757c794
fasiondog <fasiondog@163.com>
2025-08-14 00:28:48 +0800

    feat(hikyuu_pywrap): 完善 register_extra_ktype 函数文档

--- hikyuu_pywrap/plugin/_hkuextra.cpp
+++ hikyuu_pywrap/plugin/_hkuextra.cpp
@@ -22,7 +22,24 @@ void export_hkuextra(py::module& m) {
           py::arg("ktype"), py::arg("basetype"), py::arg("nbars"),
           R"(register_extra_ktype(ktype, basetype, nbars|minutes[, get_phase_end])
           
-    注册扩展K线类型, 实现自定义动态周期K线
+    注册扩展K线类型, 实现自定义动态周期K线。
+
+    方式一: 严格基于Bar数量进行合成, 如: register_extra_ktype("DAY4, "DAY", 4)
+    方式二: 基于时间段进行合成, 需定义当前时间计算周期结束时间的转换函数, 如定义7分钟K线:
+
+      def get_min3_phase_end(datetime):
+          m = datetime.minute
+          if m % 3 == 0:
+              return datetime
+          m = (m / 3 + 1) * 3
+          return Datetime(datetime.year, datetime.month, datetime.day, datetime.hour, m)
+
+      register_extra_ktype("MIN3", "MIN", 3, get_min3_phase_end)
+
+    注意：
+    1. Hikyuu 已内建了基于Bar数量的的DAY3、DAY5、DAY7, 和基于时间转换的 MIN3 扩展K线
+    2. 建议使用 hub c++ 部件的方式创建自定义K线类型周期结束点计算转换函数, 因为 python 带有 GIL 锁, 
+       创建转换函数时将可能导致无法使用多线程进行计算
     
     :param str ktype: 扩展K线类型名称
     :param str basetype: 基础K线类型名称
@@ -32,5 +49,5 @@ void export_hkuextra(py::module& m) {
 
     m.def("release_extra_ktype", releaseExtraKType, R"(release_ktype_extra()
         
-    释放扩展K线类型)");
+    释放扩展K线类型, 内部使用。python退出时调用, 防止定义的python转换函数导致退出崩溃)");
 }
