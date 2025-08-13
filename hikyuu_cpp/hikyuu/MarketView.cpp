bfa9ce230cae3c614ac3fc9c2fdeb6f86ae0e2e3
fasiondog <fasiondog@163.com>
2025-08-13 18:00:32 +0800

    feat(MarketView): 增加股票行情数据接口
    - 重构 MarketViewRecord 结构，增加更多行情数据字段
    - 修改 getMarketView 函数，使用市场代码获取交易日历
    - 计算并添加振幅、涨跌幅、换手率等行情指标
    - 更新 Python 接口，返回 pandas DataFrame 格式数据
    - 添加相关文档和国际化支持

--- hikyuu_cpp/hikyuu/MarketView.cpp
+++ hikyuu_cpp/hikyuu/MarketView.cpp
@@ -5,28 +5,112 @@
  *      Author: fasiondog
  */
 
+#include "hikyuu/indicator/crt/KDATA.h"
+#include "hikyuu/indicator/crt/FINANCE.h"
+#include "hikyuu/indicator/crt/HSL.h"
+#include "hikyuu/indicator/crt/ZONGGUBEN.h"
+#include "hikyuu/indicator/crt/LIUTONGPAN.h"
+#include "StockManager.h"
 #include "MarketView.h"
 
 namespace hku {
 
-string MarketViewRecord::str() const {
-    return fmt::format("{}, {}, {}, {}, {}, {}, {}, {}", stock.market_code(), stock.name(),
-                       krecord.datetime.ymdhm(), krecord.openPrice, krecord.highPrice,
-                       krecord.lowPrice, krecord.closePrice, krecord.transAmount,
-                       krecord.transCount);
-}
-
-MarketView HKU_API getMarketView(const StockList& stks, const Datetime& datetime,
-                                 const KQuery::KType& ktype) {
+MarketView HKU_API getMarketView(const StockList& stks, const string& market) {
     MarketView ret;
-    for (const auto& stk : stks) {
+    DatetimeList dates = StockManager::instance().getTradingCalendar(KQueryByIndex(-2), market);
+    HKU_IF_RETURN(dates.size() < 2, ret);  // 小于两个交易日暂时不处理
+
+    Datetime yesterday = dates[0];
+    Datetime today = dates[1];
+    Datetime tomorrow = today.nextDay();
+
+    Indicator EPS = FINANCE(0);
+    EPS.setParam("dynamic", true);
+    Indicator PE = CLOSE() / EPS;
+    Indicator PB = CLOSE() / FINANCE(3);  // 市净率
+
+    size_t total = stks.size();
+    ret.resize(stks.size());
+    size_t idx = 0;
+    for (size_t i = 0; i < total; i++) {
+        const Stock& stk = stks[i];
         if (!stk.isNull()) {
-            auto krecord = stk.getKRecord(datetime, ktype);
-            if (krecord.isValid()) {
-                ret.emplace_back(stk, krecord);
+            auto kdata = stk.getKData(KQueryByDate(yesterday, tomorrow));
+            if (kdata.size() == 2) {
+                auto& mr = ret[idx++];
+                mr.stock = stk;
+                mr.date = kdata[1].datetime;
+                mr.open = kdata[1].openPrice;
+                mr.high = kdata[1].highPrice;
+                mr.low = kdata[1].lowPrice;
+                mr.close = kdata[1].closePrice;
+                mr.amount = kdata[1].transAmount;
+                mr.volume = kdata[1].transCount;
+                mr.yesterday_close = kdata[0].closePrice;
+                Indicator::value_t hsl = HSL(kdata)[1];
+                if (hsl != Null<Indicator::value_t>()) {
+                    mr.turnover = hsl * 100.0;
+                }
+                mr.amplitude =
+                  mr.open != 0.0 ? ((mr.high - mr.low) / mr.open) * 100.0 : Null<price_t>();
+                mr.price_change = mr.yesterday_close != 0.0
+                                    ? ((mr.close - mr.yesterday_close) / mr.yesterday_close) * 100.0
+                                    : Null<price_t>();
+                Indicator::value_t zongguben = ZONGGUBEN(kdata)[1];
+                if (zongguben != Null<Indicator::value_t>()) {
+                    mr.total_market_cap = zongguben * mr.close;
+                }
+                Indicator::value_t liutongpan = LIUTONGPAN(kdata)[1];
+                if (liutongpan != Null<Indicator::value_t>()) {
+                    mr.circulating_market_cap = liutongpan * mr.close;
+                }
+                Indicator::value_t pe = PE(kdata)[1];
+                if (pe != Null<Indicator::value_t>()) {
+                    mr.pe = pe;
+                }
+                Indicator::value_t pb = PB(kdata)[1];
+                if (pb != Null<Indicator::value_t>()) {
+                    mr.pb = pb;
+                }
+            } else if (kdata.size() == 1 && kdata[0].datetime == dates[1]) {
+                auto& mr = ret[idx++];
+                mr.stock = stk;
+                mr.date = kdata[0].datetime;
+                mr.open = kdata[0].openPrice;
+                mr.high = kdata[0].highPrice;
+                mr.low = kdata[0].lowPrice;
+                mr.close = kdata[0].closePrice;
+                mr.amount = kdata[0].transAmount;
+                mr.volume = kdata[0].transCount;
+                mr.yesterday_close = Null<price_t>();
+                Indicator::value_t hsl = HSL(kdata)[0];
+                if (hsl != Null<Indicator::value_t>()) {
+                    mr.turnover = hsl * 100.0;
+                }
+                mr.amplitude =
+                  mr.open != 0.0 ? ((mr.high - mr.low) / mr.open) * 100.0 : Null<price_t>();
+                mr.price_change =
+                  mr.open != 0.0 ? ((mr.close - mr.open) / mr.open) * 100.0 : Null<price_t>();
+                Indicator::value_t zongguben = ZONGGUBEN(kdata)[0];
+                if (zongguben != Null<Indicator::value_t>()) {
+                    mr.total_market_cap = zongguben * mr.close;
+                }
+                Indicator::value_t liutongpan = LIUTONGPAN(kdata)[0];
+                if (liutongpan != Null<Indicator::value_t>()) {
+                    mr.circulating_market_cap = liutongpan * mr.close;
+                }
+                Indicator::value_t pe = PE(kdata)[0];
+                if (pe != Null<Indicator::value_t>()) {
+                    mr.pe = pe;
+                }
+                Indicator::value_t pb = PB(kdata)[0];
+                if (pb != Null<Indicator::value_t>()) {
+                    mr.pb = pb;
+                }
             }
         }
     }
+    ret.resize(idx);
     return ret;
 }
 
--- hikyuu_pywrap/_MarketView.cpp
+++ hikyuu_pywrap/_MarketView.cpp
@@ -14,10 +14,10 @@ namespace py = pybind11;
 void export_MarketView(py::module& m) {
     m.def(
       "get_market_view",
-      [](const py::sequence& stks, const Datetime& datetime, const KQuery::KType& ktype) {
+      [](const py::sequence& stks, const string& market) {
           SPEND_TIME(get_market_view);
           StockList stks_list = python_list_to_vector<Stock>(stks);
-          auto view = getMarketView(stks_list, datetime, ktype);
+          auto view = getMarketView(stks_list, market);
 
           size_t total = view.size();
           if (total == 0) {
@@ -32,6 +32,14 @@ void export_MarketView(py::module& m) {
           py::array_t<double> close_arr(total);
           py::array_t<double> amount_arr(total);
           py::array_t<double> vol_arr(total);
+          py::array_t<double> yesterday_close_arr(total);
+          py::array_t<double> price_change_arr(total);
+          py::array_t<double> amplitude_arr(total);
+          py::array_t<double> turnover_arr(total);
+          py::array_t<double> total_market_cap_arr(total);
+          py::array_t<double> circulating_market_cap_arr(total);
+          py::array_t<double> pb_arr(total);
+          py::array_t<double> pe_arr(total);
 
           // 获取缓冲区并填充数据
           auto datetime_buf = datetime_arr.request();
@@ -41,6 +49,14 @@ void export_MarketView(py::module& m) {
           auto close_buf = close_arr.request();
           auto amount_buf = amount_arr.request();
           auto vol_buf = vol_arr.request();
+          auto yesterday_close_buf = yesterday_close_arr.request();
+          auto price_change_buf = price_change_arr.request();
+          auto amplitude_buf = amplitude_arr.request();
+          auto turnover_buf = turnover_arr.request();
+          auto total_market_cap_buf = total_market_cap_arr.request();
+          auto circulating_market_cap_buf = circulating_market_cap_arr.request();
+          auto pb_buf = pb_arr.request();
+          auto pe_buf = pe_arr.request();
 
           int64_t* datetime_ptr = static_cast<int64_t*>(datetime_buf.ptr);
           double* open_ptr = static_cast<double*>(open_buf.ptr);
@@ -49,6 +65,14 @@ void export_MarketView(py::module& m) {
           double* close_ptr = static_cast<double*>(close_buf.ptr);
           double* amount_ptr = static_cast<double*>(amount_buf.ptr);
           double* vol_ptr = static_cast<double*>(vol_buf.ptr);
+          double* yesterday_close_ptr = static_cast<double*>(yesterday_close_buf.ptr);
+          double* price_change_ptr = static_cast<double*>(price_change_buf.ptr);
+          double* amplitude_ptr = static_cast<double*>(amplitude_buf.ptr);
+          double* turnover_ptr = static_cast<double*>(turnover_buf.ptr);
+          double* total_market_cap_ptr = static_cast<double*>(total_market_cap_buf.ptr);
+          double* circulating_market_cap_ptr = static_cast<double*>(circulating_market_cap_buf.ptr);
+          double* pb_ptr = static_cast<double*>(pb_buf.ptr);
+          double* pe_ptr = static_cast<double*>(pe_buf.ptr);
 
           auto pandas = py::module_::import("pandas");
           py::list code_list(total);
@@ -59,28 +83,53 @@ void export_MarketView(py::module& m) {
               code_list[i] = stock.market_code();
               name_list[i] = stock.name();
 
-              const KRecord& k = ptr[i].krecord;
-              datetime_ptr[i] = k.datetime.timestamp() * 1000LL;
-              open_ptr[i] = k.openPrice;
-              high_ptr[i] = k.highPrice;
-              low_ptr[i] = k.lowPrice;
-              close_ptr[i] = k.closePrice;
-              amount_ptr[i] = k.transAmount;
-              vol_ptr[i] = k.transCount;
+              datetime_ptr[i] = ptr[i].date.timestamp() * 1000LL;
+              open_ptr[i] = ptr[i].open;
+              high_ptr[i] = ptr[i].high;
+              low_ptr[i] = ptr[i].low;
+              close_ptr[i] = ptr[i].close;
+              amount_ptr[i] = ptr[i].amount;
+              vol_ptr[i] = ptr[i].volume;
+              yesterday_close_ptr[i] = ptr[i].yesterday_close;
+              price_change_ptr[i] = ptr[i].price_change;
+              amplitude_ptr[i] = ptr[i].amplitude;
+              turnover_ptr[i] = ptr[i].turnover;
+              total_market_cap_ptr[i] = ptr[i].total_market_cap;
+              circulating_market_cap_ptr[i] = ptr[i].circulating_market_cap;
+              pb_ptr[i] = ptr[i].pb;
+              pe_ptr[i] = ptr[i].pe;
           }
 
           py::dict columns;
-          columns["market_code"] = pandas.attr("Series")(code_list, py::arg("dtype") = "string");
-          columns["name"] = pandas.attr("Series")(name_list, py::arg("dtype") = "string");
-          columns["datetime"] = datetime_arr.attr("astype")("datetime64[ns]");
-          columns["open"] = open_arr;
-          columns["high"] = high_arr;
-          columns["low"] = low_arr;
-          columns["close"] = close_arr;
-          columns["amount"] = amount_arr;
-          columns["volume"] = vol_arr;
+          columns[htr("market_code").c_str()] =
+            pandas.attr("Series")(code_list, py::arg("dtype") = "string");
+          columns[htr("name").c_str()] =
+            pandas.attr("Series")(name_list, py::arg("dtype") = "string");
+          columns[htr("date").c_str()] = datetime_arr.attr("astype")("datetime64[ns]");
+          columns[htr("open").c_str()] = open_arr;
+          columns[htr("high").c_str()] = high_arr;
+          columns[htr("low").c_str()] = low_arr;
+          columns[htr("close").c_str()] = close_arr;
+          columns[htr("amount").c_str()] = amount_arr;
+          columns[htr("volume").c_str()] = vol_arr;
+          columns[htr("yesterday_close").c_str()] = yesterday_close_arr;
+          columns[htr("price_change(%)").c_str()] = price_change_arr;
+          columns[htr("amplitude(%)").c_str()] = amplitude_arr;
+          columns[htr("turnover(%)").c_str()] = turnover_arr;
+          columns[htr("total_market_cap(W)").c_str()] = total_market_cap_arr;
+          columns[htr("circulating_market_cap(W)").c_str()] = circulating_market_cap_arr;
+          columns[htr("pb").c_str()] = pb_arr;
+          columns[htr("pe").c_str()] = pe_arr;
 
           return pandas.attr("DataFrame")(columns, py::arg("copy") = false);
       },
-      py::arg("stks"), py::arg("datetime") = Datetime::today(), py::arg("ktype") = KQuery::DAY);
+      py::arg("stks"), py::arg("market") = "SH",
+      R"(get_market_view(stks, market) -> pandas.DataFrame
+
+    获取指定股票列表最后行情数据，不包含停牌无最后交易日的股票数据
+    
+    :param list[Stock] stks: 股票列表
+    :param str market: 市场代码
+    :return: 指定股票列表最后行情数据
+    :rtype: pandas.DataFrame)");
 }
