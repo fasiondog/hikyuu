28f273ee8c9faaeff8e370c5f81cb5f17487b808
fasiondog <fasiondog@163.com>
2025-10-01 16:17:54 +0800

    docs(dataserver): 更新addr选项的帮助信息

--- hikyuu/gui/dataserver.py
+++ hikyuu/gui/dataserver.py
@@ -11,7 +11,7 @@ import click
 
 
 @click.command()
-@click.option('--addr', '-addr', default="tcp://127.0.0.1:9201", help='行情接收处理服务地址')
+@click.option('--addr', '-addr', default="tcp://0.0.0.0:9201", help='自身对外提供的服务地址, 如需外部机器访问，请使用 tcp://0.0.0.0:port')
 @click.option('--work_num', '-n', default=2, type=int, help='行情接收处理线程数')
 @click.option('--save', '-save', default=False, type=bool, help='保存行情数据(仅支持clickhouse)')
 @click.option('--buf', '-buf', default=False, type=bool, help='缓存行情数据')
