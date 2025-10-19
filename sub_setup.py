ee4977f1b30c57971fda7b88a69c68d82b242be5
fasiondog <fasiondog@163.com>
2025-10-19 21:48:24 +0800

    prepare for 2.6.9 build(dependencies): 更新依赖包版本并移除Python 3.9支持

--- sub_setup.py
+++ sub_setup.py
@@ -115,7 +115,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
