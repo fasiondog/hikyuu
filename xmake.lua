cd4afd8a25aafec2c360430de29af18169014287
KongDong <fasiondog@163.com>
2025-11-16 22:36:43 +0800

    build(xmake): 更新 utf8proc 依赖版本至 2.11.0,   2.11.1 win下失败

--- xmake.lua
+++ xmake.lua
@@ -179,7 +179,7 @@ add_requires("nng", {system = false, configs = {NNG_ENABLE_TLS = has_config("htt
 add_requires("nlohmann_json", {system = false})
 add_requires("eigen", {system = false})
 add_requires("xxhash", {system = false})
-add_requires("utf8proc", {system = false})
+add_requires("utf8proc 2.11.0", {system = false})
 
 if has_config("http_client_zip") then
     add_requires("gzip-hpp", {system = false})
