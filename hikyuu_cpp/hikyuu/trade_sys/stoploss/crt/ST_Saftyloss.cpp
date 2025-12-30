2ffe3868f66ab6f27b436ce1c0c9fd0a6f55a860
fasiondog <fasiondog@163.com>
2025-12-30 20:32:58 +0800

    fix(trade_sys): 修复安全止损指标ST_Saftyloss在调用SAFTYLOSS函数时缺少CLOSE()参数

--- hikyuu_cpp/hikyuu/trade_sys/stoploss/crt/ST_Saftyloss.cpp
+++ hikyuu_cpp/hikyuu/trade_sys/stoploss/crt/ST_Saftyloss.cpp
@@ -11,7 +11,7 @@
 namespace hku {
 
 StoplossPtr HKU_API ST_Saftyloss(int n1, int n2, double p) {
-    Indicator op = SAFTYLOSS(n1, n2, p);
+    Indicator op = SAFTYLOSS(CLOSE(), n1, n2, p);
     auto result = make_shared<IndicatorStoploss>(op);
     result->name("Saftyloss_ST");
     return result;
