a5ca3ebf7bf2ec66db6ebea203e20351adf360ea
fasiondog <fasiondog@163.com>
2026-01-31 18:43:27 +0800

    fixed(hikyuutdx): 本地导入时未判断是否选择了板块信息下载

--- hikyuu/gui/data/UseTdxImportToH5Thread.py
+++ hikyuu/gui/data/UseTdxImportToH5Thread.py
@@ -94,8 +94,10 @@ class UseTdxImportToH5Thread(QThread):
             self.tasks.append(
                 ImportHistoryFinanceTask(self.log_queue, self.queue, self.config, dest_dir))
 
-        self.tasks.append(ImportBlockInfoTask(self.log_queue, self.queue,
-                          self.config))  # '概念板块', '地域板块'
+        if self.config.getboolean('block', 'enable', fallback=True):
+            self.tasks.append(ImportBlockInfoTask(self.log_queue, self.queue,
+                                                  self.config))  # '概念板块', '地域板块',
+
         self.tasks.append(ImportZhBond10Task(self.log_queue, self.queue, self.config))
 
         task_count = 0
